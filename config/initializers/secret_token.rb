3a3c772af2ed5b064a3bdea0caecd24b68688516
Nicklasos <nicklasos@gmail.com>
2012-10-24 11:05:05 +0300

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Offlinecourses::Application.config.secret_token = 'ec126ba7be931dd14ffc8be7f912e105374578eab8cdf1b4aa91babb018e9d497a8d1277f9fb29295efcd3dcac9f34695388c47fa487e11353b543220549e370'
